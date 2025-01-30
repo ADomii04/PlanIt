809720afedc7b53fd1ad4b05e1963b6f3320159e
ADomii04 <60566641+ADomii04@users.noreply.github.com>
2025-01-30 19:44:22 +0100

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-screen">
+    <div className="w-full h-[15%] bg-[#2b2b2b] flex flex-row">
+      <h1 className="text-4xl ml-12 mt-4">Planit</h1>
+      <Link href={"/signup"}><button className="text-2xl ml-8 mt-6 border-2 rounded-xl border-[#409ebd] bg-[#409ebd] w-[160px]">Signup</button></Link>
+      <Link href={"/signup"}><button className="text-2xl ml-8 mt-6 border-2 rounded-xl border-[#409ebd] bg-[#409ebd] w-[160px]">Signin</button></Link>
+    </div>
+    <div className="w-full h-[85%] flex flex-col justify-center items-center">
+        <h1 className="pl-32 pr-32 text-2xl text-center tracking-wider leading-loose">
+        PlanIt is a modern and user-friendly community event organizer designed to simplify the way people plan, manage, and attend events. Whether you're hosting a casual meetup, a workshop, or a large-scale community event, PlanIt provides all the tools you need in one place.</h1>
+        <button className="text-2xl mt-4 border-2 rounded-xl border-[#409ebd] bg-[#409ebd] w-[160px]">Lets Start</button>
+        <div className="flex mt-24 w-full ">
+              <div className="h-[100px] w-[33%] ml-28">
+                <h1 className="text-2xl font-bold">Create events</h1>
+              </div>
+              <div className="h-[100px] w-[33%] ml-28">
+                <h1 className="text-2xl font-bold">Invite participants</h1>
+              </div>
+              <div className="h-[100px] w-[33%] ml-28">
+                <h1 className="text-2xl font-bold">Collaborate</h1>
+              </div>   
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    </div>
     </div>
   );
 }
